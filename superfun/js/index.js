5b2db4e7b04c9c3ed422314c7a4bb315395e17af
xrwang <xwang@gsd.harvard.edu>
2016-02-21 21:46:30 -0800

    data updates

--- superfun/js/index.js
+++ superfun/js/index.js
@@ -8,12 +8,99 @@ var map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.Navigation());
 
-var geocoder = new mapboxgl.Geocoder();
-
-var geoc = map.addControl(geocoder);
 
 
-var geolocate = document.getElementById('current-location-btn');
+map.on('style.load', function () {
+    map.addSource("markers", {
+        "type": "geojson",
+        "data": superfunds
+    });
+
+    map.addLayer({
+        "id": "markers",
+        "interactive": true,
+        "type": "symbol",
+        "source": "markers",
+        "layout": {
+            "icon-image": "default_marker",
+            "icon-size": 1.25
+        },
+        "paint": {
+            /*"text-size": 10,*/
+        }
+    });
+});
+
+map.on('click', function (e) {
+    // Use featuresAt to get features within a given radius of the click event
+    // Use layer option to avoid getting results from other layers
+    map.featuresAt(e.point, {layer: 'markers', radius: 10, includeGeometry: true}, function (err, features) {
+        if (err) throw err;
+        // if there are features within the given radius of the click event,
+        // fly to the location of the click event
+        if (features.length) {
+            // Get coordinates from the symbol and center the map on those coordinates
+            // map.flyTo({center: features[0].geometry.coordinates});
+            var featureName = features[0].properties.NAME;
+            var featureHRS= features[0].properties.HRS;
+            var featureStat = features[0].properties.NPLSTAT;
+            var featureDate = features[0].properties.STATUS_DAT;
+            var tooltip = new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML('<p>' + 'Name of site: '+ featureName + '</br>'+'HRS: '+ featureHRS + '</br>' + 'Status of site: '+ featureStat +'</br>'+ 'Last status update: ' + featureDate + '</p>' )
+                .addTo(map);
+        }
+    });
+});
+
+
+// Use the same approach as above to indicate that the symbols are clickable
+// by changing the cursor style to 'pointer'.
+map.on('mousemove', function (e) {
+    map.featuresAt(e.point, {layer: 'markers', radius: 10}, function (err, features) {
+        if (err) throw err;
+        map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// var geocoder = new mapboxgl.Geocoder();
+//
+// var geoc = map.addControl(geocoder);
+//
+//
+// var geolocate = document.getElementById('current-location-btn');
+
+
+
+
+
 
 // the unsecure and decrepit html5 location getter api
 
